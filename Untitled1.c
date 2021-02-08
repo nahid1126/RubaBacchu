8a90c28c18610ba0b1cec4d25f195cea81076618
Nahid Hasan <45915292+nahid1126@users.noreply.github.com>
2021-02-08 23:34:11 +0600

    Add files via upload
    first-class normal code

---
+++ Untitled1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    /*int a,b,sum=0;
+
+    printf("Please enter two integer : ");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("The sum of a+b=%d\n\n",sum);
+
+    float f=3.1416;
+    printf("the float value is %0.2f",f);
+    */
+    int c=6;
+    printf("%d\n",++c);
+    printf("%d",c);
+    return 0;
+}
