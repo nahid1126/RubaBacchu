fdb05c91f2d55aa5c78f123d0d68aeaef882a7b6
Nahid Hasan <nahidnub1126@gmail.com>
2021-02-11 15:58:02 +0600

    this is the 2nd_class code

---
+++ 2nd_class.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    ///for Bitwise AND
+    /*
+    int a,b;
+    printf("please enter any two number : ");
+    scanf("%d%d",&a,&b);
+    printf("%d",a&b);*/
+
+    ///for Bitwise OR
+    /*
+    int a,b;
+    printf("please enter any two number : ");
+    scanf("%d%d",&a,&b);
+    printf("%d",a|b);*/
+
+    ///for Bitwise X-OR
+    /*
+    int a,b;
+    printf("please enter any two number : ");
+    scanf("%d%d",&a,&b);
+    printf("%d",a^b);*/
+
+    ///for Bitwise complement
+    /*
+    int a;
+    printf("please enter one number : ");
+    scanf("%d",&a);
+    printf("%d",~a);*/
+
+    ///for left shift
+    /*
+    int a,b;
+    printf("please enter any two number : ");
+    scanf("%d%d",&a,&b);
+    printf("%d",a>>b);*/
+
+    ///for right shift
+    /*
+    int a,b;
+    printf("please enter any two number : ");
+    scanf("%d%d",&a,&b);
+    printf("%d",a>>b);
+    */
+    return 0;
+}
