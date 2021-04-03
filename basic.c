68a1c1c9f0f8579fd69bef78b2336957ae951ad8
Nahid Hasan <nahidnub1126@gmail.com>
2021-04-03 15:40:14 +0600

    break code

---
+++ basic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    /*int i;
+    for(i=1;i<=100;i++){
+        printf("%d ",i);
+    }
+
+    printf("\n\n%d",i);  */
+
+    for(int i=1;i<=100;i+=2){
+        printf("%d ",i);
+    }
+
+    return 0;
+}
---
+++ break.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+   int i;
+   int number, sum = 0;
+
+   for (i = 1; i <= 10; ++i) {
+      printf("Enter n%d: ", i);
+      scanf("%d",&number);
+
+      // if the user enters a negative number, break the loop
+      if (number < 0) {
+         break;
+      }
+
+      sum += number; // sum = sum + number;
+   }
+
+   printf("Sum = %d", sum);
+
+   return 0;
+}
